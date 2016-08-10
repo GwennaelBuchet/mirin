ab099686aa455b4a38a68a7518bb2748199eec1c
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-08-11 01:28:17 +0200

    Add a full mock and convert temperature from Kelvin to Celcius

--- front/app/weather/weather.component.ts
+++ front/app/weather/weather.component.ts
@@ -9,14 +9,18 @@ import {WeatherService} from "./weather.service";
 @Component({
 	           selector: 'weather',
 	           templateUrl: 'app/weather/weather.component.html',
-	           styleUrls:  ['app/weather/weather.component.css']
+	           styleUrls: ['app/weather/weather.component.css']
            })
 export class WeatherComponent implements OnInit {
 
 	weather = {
-		details: 'il pleut un peu',
-		temperature: 17
+		temperature: {
+			temp: 0,
+			temp_min: 0,
+			temp_max: 0
+		}
 	};
+
 
 	constructor(private weatherService: WeatherService, private route: ActivatedRoute) {
 	}
@@ -27,12 +31,18 @@ export class WeatherComponent implements OnInit {
 
 	getCurrent() {
 		/*this.route.params.forEach((params: Params) => {
-			let city:string = +params['city'];
-			let country = +params['country'];
-			this.weatherService.getCurrent(city, country).then(w => this.weather = w);
-		});*/
+		 let city:string = +params['city'];
+		 let country = +params['country'];
+		 this.weatherService.getCurrent(city, country).then(w => this.weather = w);
+		 });*/
 
-		this.weatherService.getCurrent().then(w => this.weather = w);
+		this.weatherService.getCurrent().then(w => {
+			this.weather = w;
+			//to convert from Kelvin to Celcius :
+			this.weather.temperature.temp -= 273.15;
+			this.weather.temperature.temp_min -= 273.15;
+			this.weather.temperature.temp_max -= 273.15;
+		});
 
 	}
 }
--- front/app/weather/weather.service.ts
+++ front/app/weather/weather.service.ts
@@ -9,9 +9,26 @@ export class WeatherService {
 
 	getCurrent(city: string = 'lille', countryCode: string = 'fr') {
 		return Promise.resolve(
+			 //temporary mock :
 			{
-				details: 'il pleut un peu',
-				temperature: 17
+				"sunrise_time": 1470803403,
+				"dewpoint": null,
+				"weather_code": 802,
+				"humidex": null,
+				"status": "Clouds",
+				"clouds": 36,
+				"weather_icon_name": "03n",
+				"rain": {},
+				"sunset_time": 1470856482,
+				"heat_index": null,
+				"wind": {"deg": 92.0001, "speed": 1.31},
+				"detailed_status": "partiellement ensoleill\u00e9",
+				"humidity": 85,
+				"temperature": {"temp": 284.43, "temp_min": 280.93, "temp_max": 295.93, "temp_kf": null},
+				"reference_time": 1470869724,
+				"snow": {},
+				"visibility_distance": null,
+				"pressure": {"sea_level": null, "press": 1021.688}
 			}
 		);
 	}
