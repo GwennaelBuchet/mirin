e90e3f6e79f93fbdae0da45af49d6ffac4e3e662
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-08-11 02:33:33 +0200

    Add of Angular2 HTTP service to get weather from the back API

--- front/app/app.module.ts
+++ front/app/app.module.ts
@@ -4,14 +4,18 @@
 
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {HttpModule} from "@angular/http";
+
 import {AppComponent} from "./app.component";
 import {routing} from "./app.routing";
+
 import {WeatherComponent} from "./weather/weather.component";
 import {WeatherService} from "./weather/weather.service";
 
 @NgModule({
 	          imports: [
 		          BrowserModule,
+		          HttpModule,
 		          routing
 	          ],
 	          declarations: [
--- front/app/weather/weather.service.ts
+++ front/app/weather/weather.service.ts
@@ -3,34 +3,28 @@
  */
 
 import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import "rxjs/add/operator/toPromise";
+
 
 @Injectable()
 export class WeatherService {
 
+	private weatherUrl = 'http://0.0.0.0:8090/weather/';
+
+	constructor(private http: Http) {
+	}
+
 	getCurrent(city: string = 'lille', countryCode: string = 'fr') {
-		return Promise.resolve(
-			 //temporary mock :
-			{
-				"sunrise_time": 1470803403,
-				"dewpoint": null,
-				"weather_code": 802,
-				"humidex": null,
-				"status": "Clouds",
-				"clouds": 36,
-				"weather_icon_name": "03n",
-				"rain": {},
-				"sunset_time": 1470856482,
-				"heat_index": null,
-				"wind": {"deg": 92.0001, "speed": 1.31},
-				"detailed_status": "partiellement ensoleill\u00e9",
-				"humidity": 85,
-				"temperature": {"temp": 284.43, "temp_min": 280.93, "temp_max": 295.93, "temp_kf": null},
-				"reference_time": 1470869724,
-				"snow": {},
-				"visibility_distance": null,
-				"pressure": {"sea_level": null, "press": 1021.688}
-			}
-		);
+		return this.http.get(this.weatherUrl)
+		           .toPromise()
+		           .then(response => response.json())
+		           .catch(this.handleError);
+	}
+
+	private handleError(error: any) {
+		console.error('An error occurred', error);
+		return Promise.reject(error.message || error);
 	}
 }
 
