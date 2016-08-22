cc614873d51262a1306f027cb5e612f2e2d168af
Gwennael Buchet <gwennael.buchet@gmail.com>
2016-08-22 18:30:55 +0200

    Remove unused route

--- back/main.go
+++ back/main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"net/http"
-	"mirin/weather"
-	"github.com/rs/cors"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"mirin/weather"
+	"net/http"
 )
 
 func main() {
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:*"},
-		AllowedMethods: []string{"GET", "POST", "DELETE"},
+		AllowedOrigins:   []string{"http://localhost:*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE"},
 		AllowCredentials: true,
 	})
 
-	//mux := http.NewServeMux()
 	r := mux.NewRouter()
-	r.HandleFunc("/api/v1/weather/current", weather.GetCurrentWeatherHandler)
 	r.HandleFunc("/api/v1/weather/forecast/{nbForecast:[1-9]+}", weather.GetForecastWeatherHandler)
 
 	handler := c.Handler(r)
